014d50787ae64570f7a4564269c79008b6524582
telmoarduino <166237375+telmoarduino@users.noreply.github.com>
2024-04-06 17:57:55 +0200

    Limpiador de placa
    Este archivo .ino vacío borra el código grabado en la placa (los datos de la eeprom no)

---
+++ Limpiar_placa.ino
@@ -0,0 +1,9 @@
+/*Al cargar este programa se borrará la programación anterior existente y la placa no ejecutará nada*/
+void setup() {
+  // Este cóodigo solo se ejecutará una vez:
+}
+
+void loop() {
+  // Este código se repetirá en un bucle infinito:
+
+}
